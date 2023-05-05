851bfc72a0a852d7cbf13407a3c58dce8ebca35d
CsCodeway <cscodeway@gmail.com>
2023-05-05 14:07:27 +0530

    h-full remove

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -193,7 +193,7 @@ const ChatScreen = ({ chat, messages }) => {
         </div>
       </div>
       <div
-        className="flex-1 items-center justify-center overflow-y-auto px-5 pt-3 h-full"
+        className="flex-1 items-center justify-center overflow-y-auto px-5 pt-3"
         ref={chatRef}
       >
         <ReactScrollableFeed>{showMessages()}</ReactScrollableFeed>
