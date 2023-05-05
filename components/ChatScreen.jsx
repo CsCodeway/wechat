b1db59cbda4c0178bcb11e9c1b9cc40ddf040296
CsCodeway <cscodeway@gmail.com>
2023-05-05 15:21:29 +0530

    ee

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -141,10 +141,7 @@ const ChatScreen = ({ chat, messages }) => {
 
   return (
     <div className="flex flex-col h-screen flex-1">
-      <div
-        className="relative flex items-center p-2 bg-white border border-[#f5f5f5] text-blue-600i"
-        style={{ position: "sticky", top: 0 }}
-      >
+      <div className="flex items-center p-2 border border-[#f5f5f5] top-0 sticky">
         <ArrowLeftIcon
           width={25}
           height={25}
@@ -201,10 +198,7 @@ const ChatScreen = ({ chat, messages }) => {
       >
         <ReactScrollableFeed>{showMessages()}</ReactScrollableFeed>
       </div>
-      <div
-        className="relative flex items-center bg-white text-blue-600 border border-t-[#f5f5f5] custom-input-emoji"
-        style={{ height: "calc(100vh - 4rem)", position: "sticky", bottom: 0 }}
-      >
+      <div className="relative flex items-center text-blue-600 border border-t-[#f5f5f5] custom-input-emoji" style={{ height: "calc(100vh - 4rem)", position: "sticky" }}>
         <PlusCircleIcon
           width={30}
           height={30}
@@ -233,17 +227,17 @@ const ChatScreen = ({ chat, messages }) => {
             <p className="text-xs text-red-500 text-center">Remove</p>
           </div>
         )}
-        <InputEmoji
-          value={text}
-          onChange={setText}
-          cleanOnEnter
-          onEnter={handleOnEnter}
-          placeholder="Type a message"
-          className="w-full px-4 py-2 border-t text-lg"
-          style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
-        />
-      </div>
-    </div>
+       <InputEmoji
+      value={text}
+      onChange={setText}
+      cleanOnEnter
+      onEnter={handleOnEnter}
+      placeholder="Type a message"
+      className="w-full px-4 py-2 border-t text-lg"
+      style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
+    />
+  </div>
+</div>
   );
 };
 export default ChatScreen;
