8d4d10785f3739fdff6817642166b4aa2c4d4461
CsCodeway <cscodeway@gmail.com>
2023-05-05 15:06:58 +0530

    change-bg

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -141,7 +141,7 @@ const ChatScreen = ({ chat, messages }) => {
 
   return (
     <div className="flex flex-col h-screen flex-1">
-      <div className="flex items-center p-2 border border-[#f5f5f5]" style={{ position: "sticky", top: 0 }}>
+      <div className="flex items-center p-2 bg-white border border-[#f5f5f5]" style={{ position: "sticky", top: 0 }}>
         <ArrowLeftIcon
           width={25}
           height={25}
@@ -198,7 +198,7 @@ const ChatScreen = ({ chat, messages }) => {
       >
         <ReactScrollableFeed>{showMessages()}</ReactScrollableFeed>
       </div>
-      <div className="relative flex items-center text-blue-600 border border-t-[#f5f5f5] custom-input-emoji" style={{ height: "calc(100vh - 4rem)", position: "sticky", bottom: 0 }}>
+      <div className="relative flex items-center bg-white text-blue-600 border border-t-[#f5f5f5] custom-input-emoji" style={{ height: "calc(100vh - 4rem)", position: "sticky", bottom: 0 }}>
         <PlusCircleIcon
           width={30}
           height={30}
