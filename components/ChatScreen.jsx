20f8abc27e9e0445ea8bc69c93486c3947af465b
CsCodeway <cscodeway@gmail.com>
2023-05-05 15:01:31 +0530

    layout

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -141,7 +141,7 @@ const ChatScreen = ({ chat, messages }) => {
 
   return (
     <div className="flex flex-col h-screen flex-1">
-      <div className="flex items-center p-2 border border-[#f5f5f5] top-0 sticky">
+      <div className="flex items-center p-2 border border-[#f5f5f5]">
         <ArrowLeftIcon
           width={25}
           height={25}
@@ -198,7 +198,7 @@ const ChatScreen = ({ chat, messages }) => {
       >
         <ReactScrollableFeed>{showMessages()}</ReactScrollableFeed>
       </div>
-      <div className="relative flex items-center text-blue-600 border border-t-[#f5f5f5] custom-input-emoji" style={{ height: "calc(100vh - 4rem)" }}>
+      <div className="relative flex items-center text-blue-600 border border-t-[#f5f5f5] custom-input-emoji" style={{ height: "calc(100vh - 4rem)", position: "sticky", bottom: 0 }}>
         <PlusCircleIcon
           width={30}
           height={30}
