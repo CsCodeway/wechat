a9b20828489c49a7e40ef3f53d3c223accff53f5
CsCodeway <cscodeway@gmail.com>
2023-06-05 19:20:36 +0530

    loading set

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,14 +1,20 @@
 import BarLoader from "react-spinners/BarLoader";
 import logo from "../public/logo.png";
 import Image from "next/image";
+import Head from "next/head";
 
 const Loading = () => {
   return (
     <>
+    <Head>
+        <title>Loading...</title>
+        <meta name="description" content="wechat" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=1, maximum-scale=1"
+        />
+      </Head>
       <div className="flex items-center justify-center h-screen">
-        <div>
-          <title>Loading...</title>
-        </div>
         <div className="flex flex-col gap-y-20 items-center justify-center">
           <Image src={logo} alt="" width={150} height={150} />
           <BarLoader color="#6b79ca" width={150} />
